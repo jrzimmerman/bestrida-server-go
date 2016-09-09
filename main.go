dee5a47522f826ee1086bc551d2ae1a4299ab05f
Justin Zimmerman <zimmy130@gmail.com>
2016-09-09 07:13:34 -0400

    add manners for graceful server shutdown

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"net/http"
 	"os"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/braintree/manners"
 	"github.com/jrzimmerman/bestrida-server-go/routes"
 )
 
 func main() {
 	port := getEnvString("PORT")
 	logrus.WithField("port", port).Info("Listening for http traffic")
-	logrus.Fatal(http.ListenAndServe(":"+port, routes.API()))
+	logrus.Fatal(manners.ListenAndServe(":"+port, routes.API()))
 }
 
 func getEnvString(env string) string {
---
+++ middleware/cors.go
@@ -0,0 +1,15 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+// CORS providing support for Cross-Origin Resource Sharing.
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Content-Type", "application/json")
+
+		c.Next()
+	}
+}
