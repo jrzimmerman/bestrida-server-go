82adf805b27890bd7777dda030adf1664b6da1ee
Justin Zimmerman <zimmy130@gmail.com>
2016-09-11 09:31:34 -0400

    import logrus as log

--- main.go
+++ main.go
@@ -3,21 +3,21 @@ package main
 import (
 	"os"
 
-	"github.com/Sirupsen/logrus"
+	log "github.com/Sirupsen/logrus"
 	"github.com/braintree/manners"
 	"github.com/jrzimmerman/bestrida-server-go/routes"
 )
 
 func main() {
 	port := getEnvString("PORT")
-	logrus.WithField("port", port).Info("Listening for http traffic")
-	logrus.Fatal(manners.ListenAndServe(":"+port, routes.API()))
+	log.WithField("PORT", port).Info("Listening for http traffic")
+	log.Fatal(manners.ListenAndServe(":"+port, routes.API()))
 }
 
 func getEnvString(env string) string {
 	str, ok := os.LookupEnv(env)
 	if !ok {
-		logrus.WithField("env", env).Fatal("Missing required environment variable")
+		log.WithField("env", env).Fatal("Missing required environment variable")
 	}
 	return str
 }
