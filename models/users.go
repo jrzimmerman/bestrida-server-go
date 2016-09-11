04a980b711adfff7df68b7d0991ed8dad7ce11e7
Justin Zimmerman <zimmy130@gmail.com>
2016-09-11 11:04:30 -0400

    update users model, add get user test

--- models/users.go
+++ models/users.go
@@ -7,37 +7,37 @@ import (
 
 // Friend struct handles the MongoDB schema for each users friends
 type Friend struct {
-	ID             int    `json:"id"`
-	UserName       string `json:"userName"`
-	FirstName      string `json:"firstName"`
-	LastName       string `json:"lastName"`
-	FullName       string `json:"fullName"`
-	Photo          string `json:"photo"`
-	ChallengeCount int    `json:"challengeCount"`
-	Wins           int    `json:"wins"`
-	Losses         int    `json:"losses"`
+	ID             int    `bson:"_id" json:"id"`
+	UserName       string `bson:"username" json:"username"`
+	FirstName      string `bson:"firstName" json:"firstName"`
+	LastName       string `bson:"lastName" json:"lastName"`
+	FullName       string `bson:"fullName" json:"fullName"`
+	Photo          string `bson:"photo" json:"photo"`
+	ChallengeCount int    `bson:"challengeCount" json:"challengeCount"`
+	Wins           int    `bson:"wins" json:"wins"`
+	Losses         int    `bson:"losses" json:"losses"`
 }
 
 // UserSegment struct handles the MongoDB schema for each users segments
 type UserSegment struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Count int    `json:"count"`
+	ID    int    `bson:"_id" json:"id"`
+	Name  string `bson:"name" json:"name"`
+	Count int    `bson:"count" json:"count"`
 }
 
 // User struct handles the MongoDB schema for a user
 type User struct {
-	ID        int           `json:"_id"`
-	FirstName string        `json:"firstName"`
-	LastName  string        `json:"lastName"`
-	FullName  string        `json:"fullName"`
-	Token     string        `json:"token"`
-	Photo     string        `json:"photo"`
-	Email     string        `json:"email"`
-	Friends   []Friend      `json:"friends"`
-	Segments  []UserSegment `json:"segments"`
-	Wins      int           `json:"wins"`
-	Losses    int           `json:"losses"`
+	ID        int           `bson:"_id" json:"id"`
+	FirstName string        `bson:"firstName" json:"firstName"`
+	LastName  string        `bson:"lastName" json:"lastName"`
+	FullName  string        `bson:"fullName" json:"fullName"`
+	Token     string        `bson:"token" json:"token"`
+	Photo     string        `bson:"photo" json:"photo"`
+	Email     string        `bson:"email" json:"email"`
+	Friends   []Friend      `bson:"friends" json:"friends"`
+	Segments  []UserSegment `bson:"segments" json:"segments"`
+	Wins      int           `bson:"wins" json:"wins"`
+	Losses    int           `bson:"losses" json:"losses"`
 }
 
 // GetUserByID gets a single stored user from MongoDB
---
+++ tests/users_test.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/jrzimmerman/bestrida-server-go/models"
+	"github.com/jrzimmerman/bestrida-server-go/routes"
+)
+
+func TestGetUserByID(t *testing.T) {
+	id := 1027935
+
+	// Create the http request
+	req, err := http.NewRequest("GET", fmt.Sprintf("/api/users/%v", id), nil)
+	if err != nil {
+		t.Fatal("unable to generate request", err)
+	}
+
+	// Send the request to the API
+	rec := httptest.NewRecorder()
+	routes.API().ServeHTTP(rec, req)
+
+	// Check the status code
+	if exp := http.StatusOK; rec.Code != exp {
+		t.Fatalf("expected status code %v, got: %v", exp, rec.Code)
+	}
+
+	// Unmarshal and check the response body
+	var u models.User
+	if err := json.NewDecoder(rec.Body).Decode(&u); err != nil {
+		t.Fatalf("unable to decode response: %s", err)
+	}
+
+	log.WithField("User ID", u.ID).Info("User returned from MongoDB")
+
+	if u.ID != id {
+		t.Fatalf("unexpected user")
+	}
+}
