5b195bcc3bebd4ece273dfb70b708fdede21e1b6
Justin Zimmerman <zimmy130@gmail.com>
2016-09-15 22:17:25 -0400

    update vendor file, add segments test

---
+++ tests/segments_test.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/jrzimmerman/bestrida-server-go/handlers"
+	"github.com/jrzimmerman/bestrida-server-go/models"
+)
+
+func TestGetSegmentByID(t *testing.T) {
+	id := 2539276
+
+	// Create the http request
+	req, err := http.NewRequest("GET", fmt.Sprintf("/api/segments/%v", id), nil)
+	if err != nil {
+		t.Fatal("unable to generate request", err)
+	}
+
+	// Send the request to the API
+	rec := httptest.NewRecorder()
+	handlers.API().ServeHTTP(rec, req)
+
+	// Check the status code
+	if exp := http.StatusOK; rec.Code != exp {
+		t.Fatalf("expected status code %v, got: %v", exp, rec.Code)
+	}
+
+	// Unmarshal and check the response body
+	var s models.Segment
+	if err := json.NewDecoder(rec.Body).Decode(&s); err != nil {
+		t.Fatalf("unable to decode response: %s", err)
+	}
+
+	log.WithField("Segment ID", s.ID).Info("Segment returned from MongoDB")
+
+	if s.ID != id {
+		t.Fatalf("unexpected segment")
+	}
+}
