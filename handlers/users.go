80d513d1d112c5fceee76e3f0e089e43ed7fa968
Justin Zimmerman <zimmy130@gmail.com>
2016-10-09 14:49:15 -0400

    update users and users tests

--- handlers/users.go
+++ handlers/users.go
@@ -12,12 +12,14 @@ import (
 // GetUserByID returns user by ID from the database
 func GetUserByID(c *gin.Context) {
 	id := c.Param("id")
+
 	numID, err := strconv.Atoi(id)
 	if err != nil {
 		log.WithField("ID", numID).Error("unable to convert ID param")
 		c.JSON(500, "unable to convert ID param")
 		return
 	}
+
 	user, err := models.GetUserByID(numID)
 	if err != nil {
 		log.WithField("ID", numID).Error("unable to get user by ID")
