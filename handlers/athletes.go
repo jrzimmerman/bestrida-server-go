04c29eb04861205905c011bbd40507105d3b2f0a
Justin Zimmerman <zimmy130@gmail.com>
2016-10-11 22:01:36 -0400

    mid segments route, able to retrieve activity details to obtain segments

--- handlers/athletes.go
+++ handlers/athletes.go
@@ -73,6 +73,7 @@ func GetFriendsByUserIDFromStrava(c *gin.Context) {
 func GetSegmentsByUserIDFromStrava(c *gin.Context) {
 	id := c.Param("id")
 
+	// convert id string to number
 	numID, err := strconv.Atoi(id)
 	if err != nil {
 		log.WithField("ID", numID).Error("unable to convert ID param")
@@ -80,6 +81,7 @@ func GetSegmentsByUserIDFromStrava(c *gin.Context) {
 		return
 	}
 
+	// find user by numID to retrieve strava token
 	user, err := models.GetUserByID(numID)
 	if err != nil {
 		log.WithField("ID", numID).Error("unable to retrieve user from database")
@@ -87,13 +89,30 @@ func GetSegmentsByUserIDFromStrava(c *gin.Context) {
 		return
 	}
 
+	// create new strava client with user token
 	client := strava.NewClient(user.Token)
 
-	log.Info("Fetching athlete activity info...\n")
+	log.Info("Fetching athlete activity summary info...\n")
 	activities, err := strava.NewCurrentAthleteService(client).ListActivities().Do()
 	if err != nil {
-		c.JSON(500, "Unable to retrieve athlete activities")
+		c.JSON(500, "Unable to retrieve athlete activities summary")
 		return
+	}
+
+	// range over activity summary to get activity details
+	for _, activitySummary := range activities {
+		log.WithField("activity summary", activitySummary).Info("activity summary")
+
+		// request activity detail from strava to obtain segments
+		activityDetail, err := strava.NewActivitiesService(client).Get(activitySummary.Id).Do()
+		if err != nil {
+			log.WithField("activity detail", activityDetail).Error("unable to retrieve activity detail")
+			c.JSON(500, "Unable to retrieve activity detail")
+			return
+		}
+		log.WithField("activity detail", activityDetail).Info("activity detail")
+
+		// range over activity detail to obtain segment efforts
 	}
 
 	c.JSON(200, activities)
