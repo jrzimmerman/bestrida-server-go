984fbdf7c54bffd29186c28797e254e68e733270
Justin Zimmerman <jzimmerman@redventures.com>
2016-10-31 09:49:43 -0400

    remove redunant logging

--- handlers/strava.go
+++ handlers/strava.go
@@ -33,15 +33,7 @@ func oAuthFailure(err error, w http.ResponseWriter, r *http.Request) {
 	// some standard error checking
 	if err == strava.OAuthAuthorizationDeniedErr {
 		log.WithError(err).Error("The user clicked the 'Do not Authorize' button on the previous page.")
-		log.WithError(err).Error("This is the main error your application should handle.")
-	} else if err == strava.OAuthInvalidCredentialsErr {
-		log.WithError(err).Error("You provided an incorrect client_id or client_secret.")
-		log.WithError(err).Error("Did you remember to set them at the beginning of this file?")
-	} else if err == strava.OAuthInvalidCodeErr {
-		log.WithError(err).Error("The temporary token was not recognized, this shouldn't happen normally")
-	} else if err == strava.OAuthServerErr {
-		log.WithError(err).Error("There was some sort of server error, try again to see if the problem continues")
 	} else {
-		log.WithError(err).Error("Unknown Error Authorizing")
+		log.WithError(err).Error("Unknown Error Authorizing with Strava")
 	}
 }
