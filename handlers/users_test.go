4f172e3d0d814aafa979d850b44da06cb8ef4139
Justin Zimmerman <zimmy130@gmail.com>
2017-08-26 18:14:33 -0400

    add user handler tests

--- handlers/users_test.go
+++ handlers/users_test.go
@@ -90,3 +90,69 @@ func TestGetUserByIDFailureName(t *testing.T) {
 		t.Errorf("expected status code %v, got: %v", exp, resp.StatusCode)
 	}
 }
+
+func TestGetSegmentsByUserIDSuccess(t *testing.T) {
+	r := chi.NewRouter()
+	r.Get("/{id}", GetSegmentsByUserID)
+	server := httptest.NewServer(r)
+
+	id := 17198619
+
+	// Create the http request
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/"+strconv.Itoa(id), server.URL), nil)
+	if err != nil {
+		t.Error("unable to generate request", err)
+	}
+
+	// Send the request to the API
+	resp, err := http.DefaultClient.Do(req)
+
+	// Check the status code
+	if exp := http.StatusOK; resp.StatusCode != exp {
+		t.Errorf("expected status code %v, got: %v", exp, resp.StatusCode)
+	}
+
+	// Unmarshal and check the response body
+	var s []models.UserSegment
+	if err := json.NewDecoder(resp.Body).Decode(&s); err != nil {
+		t.Errorf("unable to decode response: %s", err)
+	}
+	log.Infof("Returned %d segments from database for user %d", len(s), id)
+
+	if len(s) <= 0 {
+		t.Errorf("no segments returned from database for user %d", id)
+	}
+}
+
+func TestGetFriendsByUserIDSuccess(t *testing.T) {
+	r := chi.NewRouter()
+	r.Get("/{id}", GetFriendsByUserID)
+	server := httptest.NewServer(r)
+
+	id := 17198619
+
+	// Create the http request
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/"+strconv.Itoa(id), server.URL), nil)
+	if err != nil {
+		t.Error("unable to generate request", err)
+	}
+
+	// Send the request to the API
+	resp, err := http.DefaultClient.Do(req)
+
+	// Check the status code
+	if exp := http.StatusOK; resp.StatusCode != exp {
+		t.Errorf("expected status code %v, got: %v", exp, resp.StatusCode)
+	}
+
+	// Unmarshal and check the response body
+	var f []models.Friend
+	if err := json.NewDecoder(resp.Body).Decode(&f); err != nil {
+		t.Errorf("unable to decode response: %s", err)
+	}
+	log.Infof("Returned %d friends from database for user %d", len(f), id)
+
+	if len(f) <= 0 {
+		t.Errorf("no segments returned from database for user %d", id)
+	}
+}
