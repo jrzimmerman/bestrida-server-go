8d337041a4b6c63890f350cd92e314a12c3e85d6
Justin Zimmerman <zimmy130@gmail.com>
2017-10-09 22:39:06 -0400

    add create challenge handler test

--- handlers/challenges_test.go
+++ handlers/challenges_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -89,5 +90,54 @@ func TestGetChallengeByIDFailureString(t *testing.T) {
 	// Check the status code
 	if exp := http.StatusInternalServerError; resp.StatusCode != exp {
 		t.Errorf("expected status code %v, got: %v", exp, resp.StatusCode)
+	}
+}
+
+func TestCreateChallengeSuccess(t *testing.T) {
+	r := chi.NewRouter()
+	r.Post("/", CreateChallenge)
+	server := httptest.NewServer(r)
+
+	var segmentID int64 = 12924664
+	byteReq := []byte(`
+		{
+			"challengerId": 17198619,
+			"challengeeId": 1027935,
+			"segmentId": 12924664,
+			"creationDate": "2016-08-28T02:38:20.926Z",
+			"completionDate": "2017-08-27T02:38:20.926Z"
+		}
+	`)
+	log.Infof("byteReq:\n%v", byteReq)
+	body := bytes.NewReader(byteReq)
+
+	// Create the http request
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/", server.URL), body)
+	if err != nil {
+		t.Error("unable to generate request", err)
+	}
+
+	// Send the request to the API
+	resp, err := http.DefaultClient.Do(req)
+
+	// Check the status code
+	if exp := http.StatusOK; resp.StatusCode != exp {
+		t.Errorf("expected status code %v, got: %v", exp, resp.StatusCode)
+	}
+
+	// Unmarshal and check the response body
+	var c models.Challenge
+	if err := json.NewDecoder(resp.Body).Decode(&c); err != nil {
+		t.Errorf("unable to decode response: %s", err)
+	}
+
+	log.WithField("Challenge ID", c.ID).Info("Challenge created")
+
+	if c.Segment.ID != segmentID {
+		t.Errorf("unexpected segment id from create challenge")
+	}
+
+	if err := models.RemoveChallenge(c.ID); err != nil {
+		t.Errorf("unable to remove challenge: %s", err)
 	}
 }
